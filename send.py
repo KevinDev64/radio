1475ea43bddcb30a62b25373cb510ab5cfbbdc11
KevinDev64 <russianbear39157@vk.com>
2023-02-12 11:42:34 +0300

    Create send.py

---
+++ send.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+import time 
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate = 38400,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+counter = 0
+
+while 1:
+    data = str.encode("Hello, World!")
+    ser.write(data)
+    print("I sent!")
+    time.sleep(1);
