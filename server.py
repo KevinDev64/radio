5e0d48c06f4febafc5a20890a25bb89f9b41451c
KevinDev64 <russianbear39157@vk.com>
2023-02-18 15:18:58 +0300

    fix 3

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ ser = serial.Serial(
     timeout = 10
 )
 readline = lambda : iter(lambda:ser.read(1),"\n")
-while "".join(readline()) != "<<SENDFILE>>": #wait for client to request file
+while "".join(readline()) != b"<<SENDFILE>>'": #wait for client to request file
     pass #do nothing ... just waiting ... we could time.sleep() if we didnt want to constantly loop
 ser.write(open("sometext.txt","rb").read()) #send file
 ser.write("\n<<EOF>>\n") #send message indicating file transmission complete
