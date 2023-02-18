5eb38b3010213e2cec9cc82fb0f56567c89825de
KevinDev64 <russianbear39157@vk.com>
2023-02-18 15:04:22 +0300

    Experimental commit

--- multi.py
+++ multi.py
@@ -9,8 +9,7 @@ ser = serial.Serial(
     baudrate = 38400,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
+    bytesize=serial.EIGHTBITS
 )
 counter = 0
 while 1:
