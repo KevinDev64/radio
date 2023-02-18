5cafcd7e1fe62279e8a10f28fd0fbb47f88e351d
KevinDev64 <russianbear39157@vk.com>
2023-02-18 15:16:11 +0300

    fix 1

--- client.py
+++ client.py
@@ -1,8 +1,15 @@
 #!/bin/python3
 
 from serial import Serial
-ser = Serial("/dev/ttyAMA0") #or whatever 
-ser.write("<<SENDFILE>>\n") #tell server we are ready to recieve
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate = 38400,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout = 10
+)
+ser.write(b'"<<SENDFILE>>\n"') #tell server we are ready to recieve
 readline = lambda : iter(lambda:ser.read(1),"\n")
 with open("sometext.txt","wb") as outfile:
    while True:
--- server.py
+++ server.py
@@ -1,7 +1,14 @@
 #!/bin/python3
 
 from serial import Serial
-ser = Serial("/dev/ttyAMA0") #or whatever 
+ser = serial.Serial(
+    port='/dev/ttyAMA0',
+    baudrate = 38400,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout = 10
+)
 readline = lambda : iter(lambda:ser.read(1),"\n")
 while "".join(readline()) != "<<SENDFILE>>": #wait for client to request file
     pass #do nothing ... just waiting ... we could time.sleep() if we didnt want to constantly loop
