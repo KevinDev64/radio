3af93e56ef73ed9ac5bb430d02c915354e80e23d
KevinDev64 <russianbear39157@vk.com>
2023-03-24 18:01:22 +0300

    fix q. sound
    3200 -> 1600

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ while True:
     if not(button.is_pressed(23)) and send == True:
         print("Button not pressed! Sending...!")
         os.system("killall -s 9 arecord")    # Останавливаем запись, если кнопка была отпущена и мы в это время отправляли
-        os.system("c2enc 3200 record.raw record.bin") # Кодируем файл
+        os.system("c2enc 1600 record.raw record.bin") # Кодируем файл
         os.remove("record.raw")  # Удаляем оригинальную запись
         
         send_file()              # Отправляем файл
@@ -81,7 +81,7 @@ while True:
         if not(recieve_file()):  # Если ничего нет в эфире, то пропускаем
             pass
         else:                    # Как только появились данные после приёма, сразу декодируем их и воспроизводим
-            os.system("c2dec 3200 sound.bin sound.raw")   # Декодируем
+            os.system("c2dec 1600 sound.bin sound.raw")   # Декодируем
             os.remove("sound.bin")                        # Удаляем закодированный файл
             os.system("aplay -D plughw:CARD=Device,DEV=0 -f S16_LE -r 48000 -t raw sound.raw")  # Воспроизводим декодированный файл
             os.remove("sound.raw")                                  # Удаляем декодированный файл
