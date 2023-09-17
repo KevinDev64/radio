1e34ac68832c94112fecfe70ab29668befd897f2
KevinDev64 <russianbear39157@vk.com>
2023-09-17 14:52:09 +0300

    add output support

--- button.py
+++ button.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import  RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import time
 
 def is_pressed(pin):
@@ -12,6 +12,19 @@ def is_pressed(pin):
     else:
         return False
 
+def power_on(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin,GPIO.HIGH)
+
+def power_off(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin,GPIO.OUT)
+    GPIO.output(pin,GPIO.LOW)
+    
 if __name__ == "__main__":    
     a = int(input())
     print(is_pressed(a))
+    
